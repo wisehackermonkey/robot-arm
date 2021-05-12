de4029b12d02a2d9118ea6dba9387d592b9d9e24
Oran C <Oranbusiness@gmail.com>
2021-05-12 15:53:24 -0700

    [add] premaid

---
+++ premaid.py
@@ -0,0 +1,28 @@
+# 3d printed Robotic Arm Project 
+# this is the code that moves the robotic arm using the pcm
+
+# by oran collins
+# github.com/wisehackermonkey
+# oranbusiness@gmail.com
+# 20210511
+
+import time
+from adafruit_servokit import ServoKit
+ 
+# Set channels to the number of servo channels on your kit.
+kit = ServoKit(channels=16)
+
+
+def move_arm(server_number, position, delay=1):
+  """this just makes the process for moving the arm easier """
+  kit.servo[server_number].angle = position
+  
+  time.sleep(delay)
+
+
+# set inital position for arm
+move_arm(0,180)
+move_arm(1,180)
+move_arm(2,180)
+move_arm(3,180)
+
