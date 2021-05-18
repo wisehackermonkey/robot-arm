6c75c363267abe36b1fe8bfb063657c6700fdeb0
Oran C <Oranbusiness@gmail.com>
2021-05-17 20:38:53 -0700

    [add] 20210517 MASS UPDATE FOR COMPUTER BACKUP Search TAG BEaNcn4ncZ

--- main.py
+++ main.py
@@ -13,24 +13,47 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 
-def move_arm(server_number, position, delay=1):
-  """this just makes the process for moving the arm easier """
-  kit.servo[server_number].angle = position
+
+def move_arm(servo_number, angle_degrees, delay_seconds=1):
+  """this just makes the process for moving the arm easier 
   
-  time.sleep(delay)
+  servo_number  = Motor number 0,1,2, or 3
+  angle_degrees = 0-180 degrees
+  delay_seconds = ex: "4" seconds motors wait for 4 sec before moving to next
+
+  EX: 
+  move_arm(0, 180, 60) : Move motor 1 to 180 degrees, then wait 1 minute
+  move_arm(3, 45, 1)   : Move motor 3 to 45 degrees, then wait 1 second
+  move_arm(2, 0, 5)    : Move motor 2 to 0 degrees, then wait 5 seconds
+  """
+  kit.servo[servo_number].angle = angle_degrees
+  
+  time.sleep(delay_seconds)
 
 
 # set inital position for arm
 move_arm(0,180)
 
-# give motion to arm
-move_arm(2, 180 - 40)
 
-# move arm back to center point 
-move_arm(2,180)
 
-# move arm to left
-move_arm(2, 180 - 30)
+#====================================
+# Edit code below here!
+#====================================
+
+
+# HERE!
+move_arm(0,180,1)
+move_arm(1,180,1)
+move_arm(2,180,1)
+move_arm(3,180,1)
+
+move_arm(0,180,1)
+move_arm(1,160,1)
+move_arm(2,140,1)
+move_arm(3,80,1)
+#====================================
+# ################################# #
+#====================================
 
 # put arm into final position
 move_arm(0,180,0)
